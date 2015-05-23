1a0b3712613b904b93ed5baea5e86c88e7b3c713
andrey0001 <andrey@sorokin.org>
2015-05-23 19:18:19 +0300

    Disabled sudo in mixmon-mp3-2.sh

--- mixmon-mp3-2.sh
+++ mixmon-mp3-2.sh
@@ -35,22 +35,22 @@ FFILENAME=${SPOOLDIR}${YEAR}/${MONTH}/${DAY}/${CALLFILENAME}.${MIXMON_FORMAT}
 WAVFILE=${FFILENAME}
 MP3FILE=`echo ${WAVFILE} | /bin/sed 's/.wav/.mp3/g'`
 
-SUDO="/usr/bin/sudo"
+#SUDO="/usr/bin/sudo"
 LOWNICE="/bin/nice -n 19 /usr/bin/ionice -c3"
 
-${SUDO} ${LOWNICE} /usr/bin/lame --quiet --preset phone -h -v ${WAVFILE} ${MP3FILE}
+${LOWNICE} /usr/bin/lame --quiet --preset phone -h -v ${WAVFILE} ${MP3FILE}
 
-${SUDO} /bin/chmod --reference=${WAVFILE} ${MP3FILE}
-${SUDO} /bin/touch --reference=${WAVFILE} ${MP3FILE}
-${SUDO} /bin/chown --reference=${WAVFILE} ${MP3FILE}
+/bin/chmod --reference=${WAVFILE} ${MP3FILE}
+/bin/touch --reference=${WAVFILE} ${MP3FILE}
+/bin/chown --reference=${WAVFILE} ${MP3FILE}
 
 /usr/bin/test -e ${MP3FILE} && ${SUDO} /bin/rm -f ${WAVFILE}
 
-${SUDO} ${LOWNICE} /usr/bin/ffmpeg -loglevel quiet -y -i ${MP3FILE} -f wav -acodec copy ${WAVFILE} >/dev/null 2>&1
+${LOWNICE} /usr/bin/ffmpeg -loglevel quiet -y -i ${MP3FILE} -f wav -acodec copy ${WAVFILE} >/dev/null 2>&1
 
-${SUDO} /bin/chmod --reference=${MP3FILE} ${WAVFILE}
-${SUDO} /bin/touch --reference=${MP3FILE} ${WAVFILE}
-${SUDO} /bin/chown --reference=${MP3FILE} ${WAVFILE}
+/bin/chmod --reference=${MP3FILE} ${WAVFILE}
+/bin/touch --reference=${MP3FILE} ${WAVFILE}
+/bin/chown --reference=${MP3FILE} ${WAVFILE}
 
 /usr/bin/test -e ${WAVFILE} && ${SUDO} /bin/rm -f ${MP3FILE}
 
