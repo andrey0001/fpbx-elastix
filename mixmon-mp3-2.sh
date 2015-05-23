7784d81078598d0090746185963c778dc11a1005
andrey0001 <andrey@sorokin.org>
2015-05-23 19:34:18 +0300

    changes in SUDO

--- mixmon-mp3-2.sh
+++ mixmon-mp3-2.sh
@@ -40,17 +40,17 @@ LOWNICE="/bin/nice -n 19 /usr/bin/ionice -c3"
 
 ${SUDO} ${LOWNICE} /usr/bin/lame --quiet --preset phone -h -v ${WAVFILE} ${MP3FILE}
 
+${SUDO} /bin/chown --reference=${WAVFILE} ${MP3FILE}
 /bin/chmod --reference=${WAVFILE} ${MP3FILE}
 /bin/touch --reference=${WAVFILE} ${MP3FILE}
-/bin/chown --reference=${WAVFILE} ${MP3FILE}
 
 /usr/bin/test -e ${MP3FILE} && /bin/rm -f ${WAVFILE}
 
 ${SUDO} ${LOWNICE} /usr/bin/ffmpeg -loglevel quiet -y -i ${MP3FILE} -f wav -acodec copy ${WAVFILE} >/dev/null 2>&1
 
+${SUDO} /bin/chown --reference=${MP3FILE} ${WAVFILE}
 /bin/chmod --reference=${MP3FILE} ${WAVFILE}
 /bin/touch --reference=${MP3FILE} ${WAVFILE}
-/bin/chown --reference=${MP3FILE} ${WAVFILE}
 
 /usr/bin/test -e ${WAVFILE} && /bin/rm -f ${MP3FILE}
 
